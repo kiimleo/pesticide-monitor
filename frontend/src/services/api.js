674028b22f6c1e392516be245559041246102416
kiimleo <kingleo.kim@gmail.com>
2024-11-11 01:15:46 +0900

    Add API_BASE_URL console log for debug

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000/api';
+console.log('API_BASE_URL:', API_BASE_URL);
 const PUBCHEM_BASE_URL = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug';
 
 
