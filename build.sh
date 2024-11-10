4c84be9143d8f699f4bb37a1e81c8b22f7e28932
kiimleo <kingleo.kim@gmail.com>
2024-11-10 20:26:43 +0900

    Update for 1st trial false

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 set -o errexit  # 오류 발생 시 스크립트 종료
 
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
-python manage.py migrate
+python pesticide_project/manage.py collectstatic --no-input
+python pesticide_project/manage.py migrate
