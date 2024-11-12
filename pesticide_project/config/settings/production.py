8f3dddf14adf2b6828b2baa2f45cac4cabeaed30
kiimleo <kingleo.kim@gmail.com>
2024-11-13 00:38:21 +0900

    Update production settings with REST Framework config. and CORS domains

--- pesticide_project/config/settings/production.py
+++ pesticide_project/config/settings/production.py
@@ -84,3 +84,14 @@ LOGGING = {
 }
 
 ROOT_URLCONF = 'config.urls'
+
+# REST Framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',  # 이 부분이 중요합니다
+    ]
+}
