620e638376a97fedcc8801bafcdfe18ef19098bc
kiimleo <kingleo.kim@gmail.com>
2024-11-14 09:31:38 +0900

    Register models in Django admin

--- pesticide_project/api/admin.py
+++ pesticide_project/api/admin.py
@@ -1,6 +1,6 @@
 # api/admin.py
 from django.contrib import admin
-from .models import SearchLog
+from .models import PesticideLimit, LimitConditionCode, SearchLog
 
 @admin.register(SearchLog)
 class SearchLogAdmin(admin.ModelAdmin):
@@ -8,3 +8,13 @@ class SearchLogAdmin(admin.ModelAdmin):
     list_filter = ('timestamp', 'results_count')
     search_fields = ('search_term', 'pesticide_term', 'food_term')
     date_hierarchy = 'timestamp'
+
+@admin.register(PesticideLimit)
+class PesticideLimitAdmin(admin.ModelAdmin):
+    list_display = ('pesticide_name_kr', 'pesticide_name_en', 'food_name', 'max_residue_limit')
+    search_fields = ('pesticide_name_kr', 'pesticide_name_en', 'food_name')
+    list_filter = ('condition_code',)
+
+@admin.register(LimitConditionCode)
+class LimitConditionCodeAdmin(admin.ModelAdmin):
+    list_display = ('code', 'description')
